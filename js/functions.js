8fcdc19dcf61900abeb8237d0c8f3bc1c9e2722c
Javier Rizzo <javierrizzoa@gmail.com>
2015-01-29 21:48:21 -0500

    Fix carousel sliding

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,14 @@
 var $root = $('html, body');
 $('a').click(function() {
         var href = $.attr(this, 'href');
+        if(href !== "#myCarousel") {
             $root.animate({
                         scrollTop: $(href).offset().top
                     }, 500, function () {
                                 window.location.hash = href;
                                     });
                 return false;
+        }
 });
 
 /*
